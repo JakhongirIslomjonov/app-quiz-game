49b970e9a2cb2ce0238a32ac6b303c7de74089ad
Jakhongir Islomjonov <jahongirnamti@gmail.com>
2024-07-26 19:40:54 +0500

    first empty project

---
+++ src/main/java/uz/pdp/appquizgame/AppQuizGameApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.appquizgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppQuizGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppQuizGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/appquizgame/AppQuizGameApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.appquizgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppQuizGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
